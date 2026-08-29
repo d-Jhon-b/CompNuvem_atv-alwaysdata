ec79976ecf0c93028f757a573666875020304301
d-Jhon-b <wwluiza.09@gmail.com>
2026-08-29 12:23:43 -0300

    fix(vite.config.ts)

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,7 +2,8 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true'
 export default defineConfig({
   plugins: [react()],
-  base: '/CompNuvem_atv-alwaysdata/',
+  base: isGitHubPages?'/CompNuvem_atv-alwaysdata/': '/',
 })
